94ce68c1a729227f6ff675d72b1bb6c5e26fc65d
Jan Houben <jan@et.tc>
2018-05-01 18:54:38 +0200

    initial commit

---
+++ generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# PHP tags to generate
+php_versions=("5.6" "7.0" "7.1" "latest")
+
+for php_version in "${php_versions[@]}"; do
+    target_dir="${script_dir}/generated/php-${php_version}"
+    mkdir -p ${target_dir}
+    cp "${script_dir}/src/Dockerfile" "${target_dir}/"
+    sed "s/#TAG#/${php_version}/" -i "${target_dir}/Dockerfile"
+done
